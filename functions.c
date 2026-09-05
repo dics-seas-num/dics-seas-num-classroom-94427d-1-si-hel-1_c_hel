874e9fdf212757f9ec64d4b8260a457d91ff6ee4
KhasarGanbat <25b1num3443@stud.num.edu.mn>
2026-09-05 23:30:45 +0800

    Refactor functions for array manipulation

--- functions.c
+++ functions.c
@@ -2,52 +2,107 @@
 
 void read(int A[], int n)
 {
-        int i;
-        for (i = 0; i < n; i++)
-                scanf("%d", &A[i]);
+    int i;
+    for (i = 0; i < n; i++)
+        scanf("%d", &A[i]);
 }
 
 void print(int A[], int n)
 {
-        int i;
-        for (i = 0; i < n; i++)
-                printf("%d ", A[i]);
-        printf("\n");
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
 }
 
 int max(int A[], int n)
 {
-        // Энд функцыг хэрэгжүүл
+    int m = A[0];
+    int i;
+    for (i = 1; i < n; i++)
+    {
+        if (A[i] > m)
+            m = A[i];
+    }
+    return m;
 }
 
 int min(int A[], int n)
 {
-        // Энд функцыг хэрэгжүүл        
+    int m = A[0];
+    int i;
+    for (i = 1; i < n; i++)
+    {
+        if (A[i] < m)
+            m = A[i];
+    }
+    return m;
 }
 
 void copy(int A[], int n, int B[])
 {
-        // Энд функцыг хэрэгжүүл        
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        B[i] = A[i];
+    }
 }
 
 int find(int A[], int n, int x)
 {
-        // Энд функцыг хэрэгжүүл        
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (A[i] == x)
+            return i;
+    }
+    return -1;
 }
 
 int make_set(int A[], int n, int B[])
 {
-        // Энд функцыг хэрэгжүүл        
+    int count = 0;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (find(B, count, A[i]) == -1)
+        {
+            B[count] = A[i];
+            count++;
+        }
+    }
+    return count;
 }
 
 int union_set(int A[], int n, int B[], int m)
 {
-        // Энд функцыг хэрэгжүүл        
+    int i;
+    int count = n;
+    for (i = 0; i < m; i++)
+    {
+        if (find(A, count, B[i]) == -1)
+        {
+            A[count] = B[i];
+            count++;
+        }
+    }
+    return count;
 }
 
 int intersection_set(int A[], int n, int B[], int m, int C[])
 {
-        // Энд функцыг хэрэгжүүл
-        
+    int count = 0;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (find(B, m, A[i]) != -1)
+        {
+            if (find(C, count, A[i]) == -1)
+            {
+                C[count] = A[i];
+                count++;
+            }
+        }
+    }
+    return count;
 }
-
